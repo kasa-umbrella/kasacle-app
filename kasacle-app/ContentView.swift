a9ebacf250aedbf640e2810d632ba732f672d670
kasatata <nk1101ruvt@gmail.com>
2026-02-13 15:07:04 +0900

    Initial Commit

---
+++ kasacle-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kasacle-app
+//
+//  Created by 笠原涼太 on 2026/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
