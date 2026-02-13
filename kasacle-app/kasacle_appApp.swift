0ff7ca970e96969d485cbf5bb5640d0b06060fe4
kasa-umbrella <nk1101ruvt@gmail.com>
2026-02-13 15:07:04 +0900

    Initial Commit

---
+++ kasacle-app/kasacle_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  kasacle_appApp.swift
+//  kasacle-app
+//
+//  Created by 笠原涼太 on 2026/02/13.
+//
+
+import SwiftUI
+
+@main
+struct kasacle_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
