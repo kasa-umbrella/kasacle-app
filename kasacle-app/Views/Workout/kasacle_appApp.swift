bcd34b9352a1d88b5bc1e9c6bbcda852b3a78a39
kasa-umbrella <nk1101ruvt@gmail.com>
2026-02-18 00:40:45 +0900

    ワークアウト機能の実装：メイン画面、ワークアウト開始画面、ワークアウト記録モデルを追加

---
+++ kasacle-app/Views/Workout/kasacle_appApp.swift
@@ -0,0 +1,19 @@
+//
+//  kasacle_appApp.swift
+//  kasacle-app
+//
+//  Created by 笠原涼太 on 2026/02/13.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct kasacle_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+        .modelContainer(for: [WorkoutRecord.self, WorkoutSession.self, WorkoutSet.self])
+    }
+}
